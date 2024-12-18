e23497c8b75bc814caceaeef2231396cb673068b
Nishita Rani <92663360+Nishita0807@users.noreply.github.com>
2024-12-18 20:56:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const model = document.getElementById('animated-model');
-  
-    // Load the 3D model using GLTFLoader
-    const loader = new THREE.GLTFLoader();
-    loader.load('assets/animation.glb', (gltf) => {
-      model.object3D.add(gltf.scene); // Add the loaded model to the AR marker
-      console.log('3D model loaded successfully!');
-    }, undefined, (error) => {
-      console.error('Error loading 3D model:', error);
-    });
+  const model = document.getElementById('animated-model');
+
+  // Load the 3D model using GLTFLoader
+  const loader = new THREE.GLTFLoader();
+  loader.load('assets/animation.glb', (gltf) => {
+    model.object3D.add(gltf.scene); // Add the loaded model to the AR marker
+    console.log('3D model loaded successfully!');
+  }, undefined, (error) => {
+    console.error('Error loading 3D model:', error);
   });
-  
+});
